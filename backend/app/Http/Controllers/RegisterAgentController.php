6409e4e86aac53acaf2130ef5f2cbce0881f5db5
sarthakCybage <154418518+sarthakCybage@users.noreply.github.com>
2024-03-26 10:55:21 +0530

    ROOM-16 Updated RegisterAgentController

--- backend/app/Http/Controllers/RegisterAgentController.php
+++ backend/app/Http/Controllers/RegisterAgentController.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
-
 use Illuminate\Http\Request;
 
 class RegisterAgentController extends Controller
